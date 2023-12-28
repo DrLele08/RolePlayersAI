0dc41e0b92d1b1caed900da3fe777229829489f9
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-12-28 10:44:18 +0100

    feat: implementazione db.define model Creazione

---
+++ Server/models/creazione.js
@@ -0,0 +1,59 @@
+const db=require("./database");
+const DataTypes=require("sequelize").DataTypes;
+const utente = require("./utente"); //bisogna crearlo
+
+const creazione = {};
+
+
+const Creazione = db.define('Creazione', {
+    id: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    utente_id: {    //Bisogna creare il model Utente
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        references:{
+            model: utente.Utente,
+            key: 'id'
+        }
+    },
+    nome:{
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    immagine:{
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    descrizione:{
+        type: DataTypes.STRING(512),
+        allowNull: false
+    },
+    is_pubblico:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    tipo:{
+        type: DataTypes.ENUM('Personaggio', 'Ambiente'),
+        allowNull: false
+    },
+    sesso:{
+        type: DataTypes.ENUM('Uomo', 'Donna', 'Altro'),
+        allowNull: true
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
+//Associazioni
+Creazione.belongsTo(utente.Utente, { //Bisogna creare il model Utente
+    foreignKey: 'utente_id',
+    as: 'Creatore'
+});
+
+
+module.exports=creazione;
