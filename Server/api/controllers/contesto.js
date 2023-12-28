5c7663b31133a6ac3210b7f60de7ee3fc5b76247
Kid-John <g.citro51@studenti.unisa.it>
2023-12-28 19:44:36 +0100

    feat: implementazione controller e route per Contesto GetById e DeleteContesto

---
+++ Server/api/controllers/contesto.js
@@ -0,0 +1,33 @@
+const contestoService = require("../../services/contestoService");
+
+exports.GetById= async (req,ris)=> {
+    let json = {};
+    let idContesto = req.params.idContesto;
+    try {
+        const contesto = await contestoService.getById(idContesto);
+        json.Ris = 1;
+        json.Mess = "OK";
+        json.Contesto = contesto;
+        ris.json(json);
+    } catch (error) {
+        json.Ris = 0;
+        json.Mess = error.message || "Errore Generico";
+        ris.json(json);
+    }
+};
+
+exports.DeleteContesto= async (req,ris)=> {
+    let json = {};
+    let idContesto = req.params.idContesto;
+    try {
+        const contesto = await contestoService.deleteContesto(idContesto);
+        json.Ris = 1;
+        json.Mess = "OK";
+        json.Contesto = contesto;
+        ris.json(json);
+    } catch (error) {
+        json.Ris = 0;
+        json.Mess = error.message || "Errore Generico";
+        ris.json(json);
+    }
+};
