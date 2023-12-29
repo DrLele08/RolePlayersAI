1632a429b32c1bcc15e6eaf8e736662491bb4597
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-12-29 11:11:21 +0100

    fix: modifica funzione checkParameter

--- Server/models/utils.js
+++ Server/models/utils.js
@@ -1,19 +1,40 @@
 const utils={};
 
-utils.checkParameter=(req,vettParam)=>{
-    for(let i=0;i<vettParam.length;i++)
-    {
-        if(req[vettParam[i]] === undefined)
-            return false;
+/**
+ * Verifica la validità dei dati forniti in base ai campi richiesti
+ *
+ * @function
+ *
+ * @param {Object} dati - Oggetto contenente i dati da verificare
+ * @param {String[]} requiredFields - Array contenente i nomi dei campi richiesti
+ * @return {boolean} - True se i dati sono validi, False altrimenti
+ */
+utils.checkParameter=(dati ,requiredFields)=>{
+    if(!dati){
+        return false;
     }
+
+    for(const field of requiredFields){
+        if(!(field in dati) || dati[field]===undefined || dati[field]===null){
+            return false;
+        }
+    }
+
     return true;
 };
 
-//Controlla la validità dell`ID
+/**
+ * Verifica la validità dell`ID
+ *
+ * @param {Number} id - ID da verificare
+ * @return {boolean} - True se l`ID è valido, False altrimenti
+ */
 utils.checkId=(id)=>{
     return id > 0;
 }
 
+
+//TODO eliminare questa funzione (anche nel service Contesto) e utilizzare checkParameter
 utils.isValidDatiContesto=(dati)=>{
     if(!dati){
         return false;
