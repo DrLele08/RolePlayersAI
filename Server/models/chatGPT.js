bed5b816c8b43c380167508e0ae196d40e0c6b4d
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-03 15:22:09 +0100

    feat: aggiunto model chatGPT

---
+++ Server/models/chatGPT.js
@@ -0,0 +1,42 @@
+const OpenAI = require("openai").OpenAI;
+
+const openai = new OpenAI({
+    apiKey: process.env.CHAT_GPT_KEY
+});
+
+const chatGPT={};
+
+
+/**
+ * Invia una richiesta di completamento chat ad OpenAI GPT-3.5 Turbo
+ * @async
+ * @function
+ * @param {Array} listaMessaggi - Una lista di messaggi nella conversazione
+ * @return {Promise<>} Una promise che si risolve con la risposta generata
+ *
+ * @example
+ * const messaggi = [
+ *   { role: "system", content: "Utente inizia la conversazione." },
+ *   { role: "utente", content: "Qual è il significato della vita?" },
+ *   { role: "assistente", content: "Il significato della vita è..." }
+ * ];
+ * try {
+ *   const risultato = await chatGPT.inviaMessaggio(messaggi);
+ *   console.log(risultato);
+ * } catch (errore) {
+ *   console.error(errore);
+ * }
+ *
+ */
+chatGPT.inviaMessaggio=async(listaMessaggi)=>{
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: listaMessaggi
+    });
+    if(chatCompletion.choices !== undefined && chatCompletion.choices.length>0) {
+        return chatCompletion.choices[0].message;
+    }
+    else {
+        return Promise.reject("Errore CHAT GPT");
+    }
+};
