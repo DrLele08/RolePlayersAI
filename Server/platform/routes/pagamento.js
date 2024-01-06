0baca177abd55b7fe69bfdd5551ea2ea48cec1ff
Kid-John <g.citro51@studenti.unisa.it>
2024-01-06 12:58:06 +0100

    fix: modifiche pagamento controller e route

--- Server/platform/routes/pagamento.js
+++ Server/platform/routes/pagamento.js
@@ -1,9 +1,10 @@
 module.exports=app=>{
     const router=require("express").Router();
+    const middlewareAuth=require("../../middleware/authApi");
     const pagamento=require("../controllers/pagamento");
 
     //Route per effettuare il pagamento, l'idAbbonamento sarebbe quello del DB
-    router.get("/:idAbbonamento",pagamento.EffettuaPagamento);
+    router.get("/:idAbbonamento",middlewareAuth([1,2,3]),pagamento.EffettuaPagamento);
 
     //Route per verificare la correttezza del pagamento, idPagamento è id transazione Stripe
     router.get("/verify/:idUtente/:idPagamento",pagamento.VerificaPagamento);
