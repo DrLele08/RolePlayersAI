86bddb1207aef3ed2907a92884a19a36c41021c5
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-08 11:14:49 +0100

    feat: aggiunte funzioni Conversazione Service

--- Server/models/conversazione.js
+++ Server/models/conversazione.js
@@ -28,6 +28,15 @@ const Conversazione = db.define('Conversazione', {
             model: creazione.Creazione,
             key: 'idCreazione'
         }
+    },
+    dataCreazione: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: db.literal('NOW()')
+    },
+    ultimoAvvio: {
+        type: DataTypes.DATE,
+        allowNull: false
     }
 },{
     freezeTableName: true,
@@ -43,7 +52,43 @@ const Conversazione = db.define('Conversazione', {
  */
 conversazione.getById = (idConversazione)=>{
     return Conversazione.findByPk(idConversazione);
-}
+};
+
+/**
+ * Restituisce la conversazione corrispondente alla sessione e al personaggio dati in input
+ *
+ * @function
+ * @param {Number} idSessione - ID della sessione
+ * @param {Number} idPersonaggio - ID del personaggio
+ * @return {Promise<>} Promise contenente l`istanza della conversazione trovata oppure null se non è stata trovata
+ */
+conversazione.getBySessioneAndPersonaggio = (idSessione, idPersonaggio)=>{
+    return Conversazione.findAll({
+        where: {
+            fkSessione: idSessione,
+            fkPersonaggio: idPersonaggio
+        }
+    });
+};
+
+/**
+ * Crea e salva un nuova conversazione nel database.
+ *
+ * @function
+ * @param {Object} dati - I dati della conversazione da salvare.
+ * @param {Number} dati.fkSessione - L'ID della sessione a cui appartiene la conversazione.
+ * @param {Number} dati.fkPersonaggio - L`ID del personaggio a cui è legata la conversazione.
+ * @returns {Promise<Object>} Un oggetto rappresentante la conversazione appena creata.
+ */
+conversazione.createConversazione = (dati) =>{
+    const currentDate = new Date();
+    return conversazione.create({
+        fkSessione: dati.fkSessione,
+        fkPersonaggio: dati.fkPersonaggio,
+        dataCreazione: currentDate,
+        ultimoAvvio: currentDate
+    });
+};
 
 conversazione.Conversazione=Conversazione;
 
---
+++ Server/services/conversazioneService.js
@@ -0,0 +1,70 @@
+const conversazione=require("../models/conversazione");
+const messaggio=require("../models/messaggio");
+const utils = require("../models/utils");
+
+const conversazioneService={};
+
+const requiredFields = ['fkSessione', 'fkPersonaggio'];
+
+conversazioneService.getById = async (idConversazione, idUtente) =>{
+    if(!utils.checkId(idConversazione) || !utils.checkId(idUtente)){
+        return Promise.reject("ID non valido");
+    }
+
+    const conv = await conversazione.getById(idConversazione);
+    if(conv === null){
+        return Promise.reject("Conversazione non trovata")
+    }
+
+    if(!await checkUtente(conv, idUtente)){
+        return Promise.reject("La conversazione appartiene ad un altro utente");
+    }
+    else{
+        return conv;
+    }
+};
+
+conversazioneService.getMessaggiByConversazione = async (idConversazione, idUtente) =>{
+    if(!utils.checkId(idConversazione) || !utils.checkId(idUtente)){
+        return Promise.reject("ID non valido");
+    }
+
+    const conv = await conversazione.getById(idConversazione);
+    if(conv === null){
+        return Promise.reject("Conversazione non trovata")
+    }
+
+    if(!await checkUtente(conv, idUtente)){
+        return Promise.reject("La conversazione appartiene ad un altro utente");
+    }
+
+    return await messaggio.getByConversazione(idConversazione);
+};
+
+conversazioneService.createConversazione = async (dati)=>{
+    if(!utils.checkParameters(dati, requiredFields)){
+        return Promise.reject("Dati non validi");
+    }
+
+    if(!utils.checkId(dati.fkSessione) || !utils.checkId(dati.fkPersonaggio)){
+        return Promise.reject("ID non validi");
+    }
+
+    //Prima di crearla controlla se esiste già una conversazione con quel personaggio in quella sessione
+    const conv = await conversazione.getBySessioneAndPersonaggio(dati.fkSessione, dati.fkPersonaggio);
+    if(conv !== null){
+        return Promise.reject("Conversazione gia esistente");
+    }
+    else{
+        return await conversazione.createConversazione(dati);
+    }
+};
+
+
+//Controlla se la conversazione appartiene ad una sessione dell`utente
+async function checkUtente(conversazione, idUtente) {
+    let sessione = await conversazione.getSessione();
+    return sessione.fkUtente === idUtente;
+}
+
+module.exports=conversazioneService;
