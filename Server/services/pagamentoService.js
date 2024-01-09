5b9a9013147e6b1bbc3d2688d0256e513a813cdc
Kid-John <g.citro51@studenti.unisa.it>
2024-01-09 17:05:21 +0100

    fix: cambio parametro di input in effettuapagamento

--- Server/services/pagamentoService.js
+++ Server/services/pagamentoService.js
@@ -1,8 +1,7 @@
-const stripe = require('stripe')('sk_test_51OURuIHOFfOlBPkf5Zoi0O0G9o3OmzAHZ3plZCPzBpa2C8PYevvdYc9DgAHKmG1dqHGvhEfAzihtwUc1zPjabuRb00i0nGIHy3');
 const utils = require("../models/utils");
-const utente = require("../models/utente");
 const abbonamento = require("../models/abbonamento");
 const stripeAdapter = require("./stripeAdapter");
+const paymentStripe = new stripeAdapter();
 
 const pagamentoService = {}
 
@@ -20,7 +19,7 @@ pagamentoService.effettuaPagamento = async(idUtente, idAbbonamento) => {
     }
 
     if(nuovoAbbonamento.prezzo > 0) {
-        return await stripeAdapter.effettuaPagamento(idUtente, abbonamento);
+        return paymentStripe.effettuaPagamento(idUtente, nuovoAbbonamento);
     }
     else{
         return {success: false, sessionUrl: "Abbonamento non valido"};
@@ -29,26 +28,7 @@ pagamentoService.effettuaPagamento = async(idUtente, idAbbonamento) => {
 
 
 pagamentoService.verificaPagamento = async(idUtente, idPagamento)=> {
-    const session = await stripe.checkout.sessions.retrieve(idPagamento);
-
-    try {
-        let idAbbonamento = session.metadata.idAbbonamento
-
-        console.log("Pagamento: " + session.payment_status);
-        console.log(idUtente);
-
-        if (session.payment_status === "paid") {
-            // Tutto ok, cambiare tipo abbonamento
-            await pagamentoService.cambiaAbbonamento(idUtente, idAbbonamento);
-            return {success: true, message: "Cambio abbonamento effettuato con successo."};
-        } else {
-            // Errore, non hai pagato
-            return {success: false, message: "Errore: il pagamento non è stato effettuato."};
-        }
-    }
-    catch(error){
-        return{success:false, message: "Errore: il pagamento non è stato effettuato."};
-    }
+    return paymentStripe.verificaPagamento(idUtente,idPagamento);
 }
 
 module.exports=pagamentoService;
