e662352b6da1a0d1b1f13c7e61239a4516e5576a
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-11 11:57:46 +0100

    feat: JSDoc conversazione

--- Server/models/messaggio.js
+++ Server/models/messaggio.js
@@ -4,9 +4,27 @@ const conversazione = require("./conversazione");
 const moment = require("moment-timezone");
 
 const PAGE_SIZE = 16;
+
+/**
+ * Oggetto contenente funzioni per la gestione dei messaggi nel database.
+ * @namespace
+ * @property {Function} getById - Ottiene un messaggio dal database tramite il suo ID.
+ * @property {Function} getByConversazione - Ottiene tutti i messaggi di una conversazione dal database.
+ * @property {Function} getByConversazionePaginated - Ottiene i messaggi paginati di una conversazione dal database.
+ * @property {Function} createMessaggio - Crea un nuovo messaggio nel database.
+ * @property {Messaggio} Messaggio - Modello Sequelize del messaggio.
+ */
 const messaggio = {}
 
-
+/**
+ * Oggetto rappresentante un messaggio nel sistema.
+ * @typedef {Object} Messaggio
+ * @property {number} idMessaggio - Identificativo univoco del messaggio (autoincrementato).
+ * @property {number} fkConversazione - Chiave esterna che collega il messaggio a una conversazione.
+ * @property {string} corpo - Contenuto del messaggio.
+ * @property {string} dataInvio - Data e ora di invio del messaggio (formattata secondo il fuso orario corrente).
+ * @property {boolean} isUtente - Indica se il messaggio è stato inviato dall`utente.
+ */
 const Messaggio = db.define('Messaggio', {
     idMessaggio: {
         type: DataTypes.BIGINT,
@@ -46,7 +64,6 @@ const Messaggio = db.define('Messaggio', {
 
 /**
  * Ottiene il messaggio con l`id dato in input
- *
  * @function
  * @param {Number} idMessaggio - ID del messaggio da cercare
  * @return {Promise<Messaggio>} Promise che restituisce il messaggio trovato o null se non è stato trovato
@@ -57,7 +74,6 @@ messaggio.getById = (idMessaggio)=>{
 
 /**
  * Ottiene tutti i messaggi di una conversazione ordinati per data di invio
- *
  * @function
  * @param {Number} idConversazione - ID della conversazione per cui ottenere i messaggi
  * @return {Promise<Array>} Un array di oggetti rappresentanti i messaggi ordinati per data
@@ -73,7 +89,6 @@ messaggio.getByConversazione = (idConversazione)=>{
 
 /**
  * Restituisce i messaggi di una conversazione paginati e ordinati per data di invio
- *
  * @function
  * @async
  * @param {Number} idConversazione - ID della conversazione per cui ottenere i messaggi
@@ -84,7 +99,7 @@ messaggio.getByConversazione = (idConversazione)=>{
  *      totalPages: Number,
  *      pageSize: Number,
  *      currentPage: Number
- *      }>} - Promise che si risolve con un oggetto contenente informazioni sulla paginazione e i messaggi della pagina richiesta
+ *      }>} - un oggetto contenente informazioni sulla paginazione e i messaggi della pagina richiesta
  */
 messaggio.getByConversazionePaginated = async (idConversazione, page) => {
     const offset = (page - 1) * PAGE_SIZE;
@@ -111,7 +126,6 @@ messaggio.getByConversazionePaginated = async (idConversazione, page) => {
 
 /**
  * Crea e salva un nuovo messaggio nel database.
- *
  * @function
  * @param {Object} dati - I dati del messaggio da salvare.
  * @param {Number} dati.fkConversazione - L'ID della conversazione a cui appartiene il messaggio.
--- Server/services/conversazioneService.js
+++ Server/services/conversazioneService.js
@@ -2,10 +2,28 @@ const conversazione=require("../models/conversazione");
 const messaggio=require("../models/messaggio");
 const utils = require("../models/utils");
 
+/**
+ * Oggetto contenente funzioni per la gestione delle conversazioni.
+ * @namespace
+ * @property {Function} getById - Ottiene una conversazione dal database tramite il suo ID.
+ * @property {Function} getMessages - Ottiene i messaggi di una conversazione paginati.
+ * @property {Function} createConversazione - Crea una nuova conversazione nel database.
+ */
 const conversazioneService={};
 
 const requiredFields = ['fkSessione', 'fkPersonaggio'];
 
+/**
+ * Ottiene una conversazione dal database tramite il suo ID verificando che appartenga all`utente specificato.
+ * @async
+ * @function
+ * @param {number} idConversazione - Identificativo univoco della conversazione.
+ * @param {number} idUtente - Identificativo univoco dell'utente.
+ * @returns {Promise<Object>} La conversazione se trovata e appartenente all'utente, altrimenti viene restituito un errore.
+ * @throws {Error} se l'ID della conversazione o dell'utente non è valido
+ * @throws {Error} se la conversazione non è stata trovata.
+ * @throws {Error} se la conversazione appartiene ad un altro utente
+ */
 conversazioneService.getById = async (idConversazione, idUtente) =>{
     if(!utils.checkId(idConversazione) || !utils.checkId(idUtente)){
         return Promise.reject("ID non valido");
@@ -24,6 +42,18 @@ conversazioneService.getById = async (idConversazione, idUtente) =>{
     }
 };
 
+/**
+ * Ottiene i messaggi di una conversazione paginati verificando che la conversazione appartenga all`utente specificato.
+ * @async
+ * @function
+ * @param {number} idConversazione - Identificativo univoco della conversazione.
+ * @param {number} pagina - Numero di pagina per la paginazione.
+ * @param {number} idUtente - Identificativo univoco dell'utente.
+ * @returns {Promise<Array<Object>>} Un array contenente i messaggi paginati della conversazione.
+ * @throws {Error} se l'ID della conversazione o dell'utente non è valido.
+ * @throws {Error} se la conversazione non è stata trovata.
+ * @throws {Error} Viene lanciato un errore se la conversazione appartiene ad un altro utente.
+ */
 conversazioneService.getMessages = async (idConversazione, pagina, idUtente) =>{
     if(!utils.checkId(idConversazione) || !utils.checkId(idUtente)){
         return Promise.reject("ID non valido");
@@ -45,6 +75,18 @@ conversazioneService.getMessages = async (idConversazione, pagina, idUtente) =>{
     return await messaggio.getByConversazionePaginated(idConversazione, pagina);
 };
 
+/**
+ * Crea una nuova conversazione nel database verificando i dati e l'esistenza di conversazioni duplicate.
+ * @async
+ * @function
+ * @param {Object} dati - Oggetto contenente i dati per la creazione della conversazione.
+ * @param {number} dati.fkSessione - Chiave esterna che collega la conversazione a una sessione.
+ * @param {number} dati.fkPersonaggio - Chiave esterna che collega la conversazione a un personaggio.
+ * @returns {Promise<Object>} La conversazione appena creata e salvata nel db.
+ * @throws {Error} Viene lanciato un errore se i dati non sono validi.
+ * @throws {Error} se gli ID non sono validi.
+ * @throws {Error} Viene lanciato un errore se esiste già una conversazione duplicata.
+ */
 conversazioneService.createConversazione = async (dati)=>{
     if(!utils.checkParameters(dati, requiredFields)){
         return Promise.reject("Dati non validi");
@@ -65,7 +107,14 @@ conversazioneService.createConversazione = async (dati)=>{
 };
 
 
-//Controlla se la conversazione appartiene ad una sessione dell`utente
+/**
+ * Controlla se una conversazione appartiene a una sessione dell'utente.
+ * @async
+ * @function
+ * @param {Object} conversazione - Oggetto rappresentante la conversazione.
+ * @param {number} idUtente - Identificativo univoco dell'utente.
+ * @returns {Promise<boolean>} True se la conversazione appartiene all'utente, altrimenti False.
+ */
 async function checkUtente(conversazione, idUtente) {
     let sessione = await conversazione.getSessione();
     return sessione.fkUtente === idUtente;
