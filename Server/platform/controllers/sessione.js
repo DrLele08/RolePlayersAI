9ec6d771e7d5fd3efaf58994cece7a667a266c03
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-16 22:34:09 +0100

    feat: aggiunta route session page

--- Server/platform/controllers/sessione.js
+++ Server/platform/controllers/sessione.js
@@ -2,7 +2,7 @@ const sessione = require("../../services/sessioneService");
 const utils = require("../../models/utils");
 const utente = require("../../services/utenteService");
 
-exports.GetConversazione = async (req, ris) => {
+exports.GetConversazione = async (req, res) => {
     try{
         const idUtente = req.idUtente;
         const idSessione = req.params.idSessione;
@@ -26,13 +26,32 @@ exports.GetConversazione = async (req, ris) => {
             });
         }
 
-        ris.render("conversazione", {
+        res.render("conversazione", {
             Utente: utils.convertToNormalObject(user),
             Abbonamento: utils.convertToNormalObject(abbonamento),
             Sessione: utils.convertToNormalObject(session),
             Conversazioni: utils.convertToNormalObject(conversazioniPersonaggi)
         });
     }catch(error){
-        ris.render("error", {errore: error});
+        res.render("error", {errore: error});
+    }
+};
+
+exports.GetSessioni = async (req, res)=>{
+    try{
+        const idUtente = req.idUtente;
+        const sessioni = await sessione.getByUtente(idUtente);
+        const user = await utente.getById(idUtente);
+        const abbonamento = await user.getAbbonamento();
+        const contesti = await user.getContestos();
+
+        res.render("gestionesessione", {
+            Utente: utils.convertToNormalObject(user),
+            Abbonamento: utils.convertToNormalObject(abbonamento),
+            Sessioni: utils.convertToNormalObject(sessioni),
+            Contesti: utils.convertToNormalObject(contesti)
+        });
+    }catch(error){
+        res.render("error", {errore: error});
     }
 };
--- Server/platform/routes/sessione.js
+++ Server/platform/routes/sessione.js
@@ -3,6 +3,8 @@ module.exports=app=>{
     const middlewareAuth=require("../../middleware/authPlatform");
     const sessione=require("../controllers/sessione");
 
+    router.get("/", middlewareAuth([1,2,3]), sessione.GetSessioni);
+
     router.get("/:idSessione", middlewareAuth([1,2,3]), sessione.GetConversazione);
 
     app.use("/sessione",router);
