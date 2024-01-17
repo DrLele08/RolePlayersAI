53adc0b8b848fd781f3237f571512811eb548a6d
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-17 16:20:31 +0100

    fix: aggiunta info utente

--- Server/platform/controllers/profilo.js
+++ Server/platform/controllers/profilo.js
@@ -1,4 +1,6 @@
 const profilo = require("../../services/profiloService");
+const utente = require("../../services/utenteService");
+const utils = require("../../models/utils");
 
 exports.GetAreaProfilo = async(req,ris)=>{
 
@@ -6,7 +8,13 @@ exports.GetAreaProfilo = async(req,ris)=>{
 
     try{
         let infoProfilo = await profilo.getInfoProfilo(idUtente);
-        ris.render("personal_area", infoProfilo);
+        const user = await utente.getById(idUtente);
+        const abbonamento = await user.getAbbonamento();
+        ris.render("personal_area", {
+            Utente: utils.convertToNormalObject(user),
+            Abbonamento: utils.convertToNormalObject(abbonamento),
+            infoProfilo: infoProfilo
+        });
     }
     catch(error){
         ris.render("error", {errore: error});
