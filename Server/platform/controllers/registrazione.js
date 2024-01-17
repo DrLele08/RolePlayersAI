5061acc0e5a72f6d6e688fc9cf0f68470f3d61f2
Stefano Guida <stefanoguida.gs@gmail.com>
2024-01-17 17:47:41 +0100

    fix: aggiunto controllo sui cookies in registrazione

--- Server/platform/controllers/registrazione.js
+++ Server/platform/controllers/registrazione.js
@@ -1,5 +1,5 @@
 exports.PaginaRegistrazione = (req, ris)=> {
-    if (req.session.idUtente == null) {
+    if (req.session.idUtente == null && req.cookies.idUtente == null) {
         ris.render("registrazione");
     } else {
         ris.redirect(process.env.BASE_URL + "/dashboard");
