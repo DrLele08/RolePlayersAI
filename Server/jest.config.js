37e5b36753ddb8f386a81fe8c51622efb372ad20
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-22 22:41:50 +0100

    feat: aggiunto jest html reporters

--- Server/jest.config.js
+++ Server/jest.config.js
@@ -2,9 +2,20 @@ module.exports = {
     setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
     reporters: [
         "default",
-        ["jest-html-reporter", {
+        ["jest-html-reporters", {
             pageTitle: "Unit Test",
             outputPath: "test-report.html"
         }]
-    ]
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: ["*/services/.{js,jsx}"],
+    coverageReporters: ["json", "lcov", "text", "clover"],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50
+        }
+    }
 };
