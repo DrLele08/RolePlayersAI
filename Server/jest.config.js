57b699b4a064cc7ed35a738388dc4296cc515b51
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-23 17:38:46 +0100

    fix: jest html reporters

--- Server/jest.config.js
+++ Server/jest.config.js
@@ -10,12 +10,4 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ["**/services/**.{js,jsx}"],
     coverageReporters: ["json", "lcov", "text", "clover"],
-    coverageThreshold: {
-        global: {
-            branches: 50,
-            functions: 50,
-            lines: 50,
-            statements: 50
-        }
-    }
 };
